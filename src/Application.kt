abfdd5a0dc4f4abb5f1372e1d01abe1984988f45
Marcelo Alban <vse_malban@uolinc.com>
2021-01-05 10:53:47 -0300

    Fix unauthorized error message

--- src/Application.kt
+++ src/Application.kt
@@ -5,11 +5,14 @@ import com.alban.todoserver.repository.TodoRepositoryImpl
 import com.alban.todoserver.repository.UserRepositoryImpl
 import com.alban.todoserver.routes.todoRoutes
 import com.alban.todoserver.routes.userRoutes
+import com.alban.todoserver.util.DataState
 import io.ktor.application.*
 import io.ktor.auth.*
 import io.ktor.auth.jwt.*
 import io.ktor.features.*
 import io.ktor.gson.*
+import io.ktor.http.*
+import io.ktor.response.*
 import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -27,6 +30,9 @@ fun Application.module() {
             validate {
                 val emailFromPayload = it.payload.getClaim("email").asString() ?: ""
                 dbUser.findUserByEmail(emailFromPayload)
+            }
+            challenge { _, _ ->
+                call.respond(HttpStatusCode.Unauthorized, DataState.Error("Unauthorized"))
             }
         }
     }
